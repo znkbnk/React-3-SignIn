38fcd2f77a61b801d4d44de53faeb26e903b3e9e
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-29 19:32:05 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignInForm from './SignInForm';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SignInForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
